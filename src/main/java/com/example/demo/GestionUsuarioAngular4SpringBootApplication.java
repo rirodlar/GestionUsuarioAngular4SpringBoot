e9157a4ea5e2f10af1be07954a1e931d761947ce
ricardo <ricardo.rodriguez.lara@gmail.com>
2018-04-11 23:25:34 -0300

    first commit

---
+++ src/main/java/com/example/demo/GestionUsuarioAngular4SpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionUsuarioAngular4SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionUsuarioAngular4SpringBootApplication.class, args);
+	}
+}
